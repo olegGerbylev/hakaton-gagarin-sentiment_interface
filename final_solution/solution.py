822613466fba614ca7af4a39c11536df869372cd
oleg <gerbylev.oleg@gmail.com>
2024-04-14 01:34:45 +0300

    init

--- final_solution/solution.py
+++ final_solution/solution.py
@@ -1,5 +1,5 @@
 import typing as tp
-
+import pandas as pd
 
 EntityScoreType = tp.Tuple[int, float]  # (entity_id, entity_score)
 MessageResultType = tp.List[
@@ -7,10 +7,53 @@ MessageResultType = tp.List[
 ]  # list of entity scores,
 #    for example, [(entity_id, entity_score) for entity_id, entity_score in entities_found]
 
+def process_text(text, company_names):
+    processed_text = text
+
+    index = processed_text.find(company_names)
+    end_index = index + 150
+    if end_index > len(text):
+        end_index = len(text)
+
+    return processed_text[index:end_index]
+
+
 
 def score_texts(
-    messages: tp.Iterable[str], *args, **kwargs
+    messages: tp.Iterable[str],
+    issuerIdAndSynonyms, loaded_model, vectorizer_loaded
 ) -> tp.Iterable[MessageResultType]:
+
+    resultIssuerId = {'items': []}
+    messageId = 0
+    resultArray = []
+    for text in messages:
+        resultArray.append([])
+        for key, subStrings in issuerIdAndSynonyms.items():
+            for subString in subStrings:
+                resString = subString
+                if len(resString) < 4:
+                    resString = " " + resString + " "
+                if resString.lower() in text.lower():
+                    resultIssuerId['items'].append({
+                        'counterId': messageId,
+                        'issuerid': key,
+                        'synonym': subString.lower(),
+                        'Company_Sentences': process_text(text.lower(), subString.lower()),
+                    })
+                    break
+        messageId+=1
+    df = pd.DataFrame(resultIssuerId['items'])
+
+    X = df['Company_Sentences']
+
+    X_test_vec = vectorizer_loaded.transform(X)
+    y_pred = loaded_model.predict(X_test_vec)
+
+    z = df['synonym']
+    for i in range(0, len(y_pred)):
+        resultArray[resultIssuerId['items'][i]['counterId']].append([resultIssuerId['items'][i]['issuerid'], float(y_pred[i])])
+    return resultArray
     """
     Main function (see tests for more clarifications)
     Args:
@@ -20,6 +63,6 @@ def score_texts(
         tp.Iterable[tp.Tuple[int, float]]: for any messages returns MessageResultType object
     -------
     Clarifications:
-    >>> assert all([len(m) < 10 ** 11 for m in messages]) # all messages are shorter than 2048 characters
-    """
-    raise NotImplementedError
+    # >>> assert all([len(m) < 10 ** 11 for m in messages]) # all messages are shorter than 2048 characters
+    # """
+    # raise NotImplementedError
--- run_me.py
+++ run_me.py
@@ -1,9 +1,13 @@
 import json
 import pathlib
 import typing as tp
+import pandas as pd
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 import final_solution
-import final_solution.solution_stupid
+
+# import final_solution.solution_stupid
 
 
 PATH_TO_TEST_DATA = pathlib.Path("data") / "test_texts.json"
@@ -13,7 +17,6 @@ PATH_TO_OUTPUT_DATA = pathlib.Path("results") / "output_scores.json"
 def load_data(path: pathlib.PosixPath = PATH_TO_TEST_DATA) -> tp.List[str]:
     with open(path, "r", encoding="utf-8") as f:
         data = json.load(f)
-
     return data
 
 
@@ -22,10 +25,22 @@ def save_data(data, path: pathlib.PosixPath = PATH_TO_OUTPUT_DATA):
         json.dump(data, f, indent=1, ensure_ascii=False)
 
 
+
 def main():
-    texts = load_data()
-    scores = final_solution.solution.score_texts(texts)
-    save_data(scores)
+    texts = load_data('data/test_texts.json')
+    nameAndSynonyms = pd.read_excel('company/names and synonyms_upd (3).xlsx')
+    issuerIdAndSynonyms = {}
+    for i, n in nameAndSynonyms.iterrows():
+        substingsArray = [x.strip() for x in n['Company_Ident_Text'].split(',') if x]
+        issuerIdAndSynonyms.update({
+            n['issuerid']: substingsArray
+        })
+    vectorizer_loaded = pickle.load(open("models/vectorizer.sav", 'rb'))
+
+    loaded_model = pickle.load(open("models/log_reg.sav", 'rb'))
+
+    scores = final_solution.solution.score_texts(texts, issuerIdAndSynonyms, loaded_model, vectorizer_loaded)
+    save_data(scores, "results/output_result.json")
 
 
 if __name__ == '__main__':
